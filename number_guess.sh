195cc6609b62c97b82f4cc138fa071075215d16d
menorton15 <menorton15@hotmail.com>
2024-04-17 04:04:36 +0000

    fix: fix game logic

--- number_guess.sh
+++ number_guess.sh
@@ -13,23 +13,25 @@ if [[ -z $USER ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
   ADD_USER_RESULT=$($PSQL "INSERT INTO users(username) values('$USERNAME')")
+  USER=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$USERNAME'")
 else
   echo $USER | while read GAMES_PLAYED BAR BEST_GAME
   do
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   done
 fi
+echo $NUMBER
 
 echo -e " \nGuess the secret number between 1 and 1000:"
 
-COUNT=0
+COUNT=1
 
 read GUESS
 
 while [[ ! $GUESS == $NUMBER ]]
 do
 
-  COUNT=($COUNT + 1)
+  COUNT=$(( $COUNT + 1 ))
 
   while [[ ! $GUESS =~ ^[0-9]+$ ]]
   do
@@ -38,22 +40,24 @@ do
     read GUESS
   done 
 
-  if [[ $GUESS > $NUMBER ]]
+  if [[ $GUESS -gt $NUMBER ]]
   then
     echo -e "\nIt's lower than that, guess again:"
     read GUESS
-  else if [[ $GUESS < $NUMBER ]]
+  elif [[ $GUESS -lt $NUMBER ]]
   then
     echo -e "\nIt's higher than that, guess again:"
     read GUESS
-  else
-    if [[ $COUNT < $BEST_GAME ]]
-    then
-      BEST_GAME=$COUNT
-    fi
-    echo -e "\nYou guessed it in $COUNT tries. The secret number was $NUMBER. Nice job!"
-
-    UPDATE_USER_DATA=$($PSQL "UPDATE users SET games_played = (games_played + 1), best_game = $BEST_GAME WHERE username = '$USERNAME'")
   fi
 done
 
+echo $USER | while read GAMES_PLAYED BAR BEST_GAME
+do
+  if [[ $COUNT -lt $BEST_GAME ]]
+  then
+    BEST_GAME=$COUNT
+  fi
+  echo "You guessed it in $COUNT tries. The secret number was $NUMBER. Nice job!"
+  GAMES_PLAYED=$(($GAMES_PLAYED + 1))
+  UPDATE_USER_DATA=$($PSQL "UPDATE users SET games_played = $GAMES_PLAYED, best_game = $BEST_GAME WHERE username = '$USERNAME'")
+done
