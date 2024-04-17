0614bf2970304cd8bc41ad210abcb8498cfb0cf9
menorton15 <menorton15@hotmail.com>
2024-04-17 02:39:00 +0000

    feat: add get user logic

--- number_guess.sh
+++ number_guess.sh
@@ -5,4 +5,19 @@ NUMBER=$((1 + $RANDOM % 1000))
 
 echo "Enter your username:"
 
+read USERNAME
+
+USER=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$USERNAME'")
+
+if [[ -z $USER ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  ADD_USER_RESULT=$($PSQL "INSERT INTO users(username) values('$USERNAME')")
+else
+  echo $USER | while read GAMES_PLAYED BAR BEST_GAME
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
+
 
